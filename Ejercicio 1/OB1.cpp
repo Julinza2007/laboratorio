aa1a77cbba141e8d16f39cbf8757b4ecdbc51bcc
julinza <julinza@example.com>
2025-03-18 22:30:03 -0300

    Agrego ejercicio 2 de Orientado a Objetos.

--- Ejercicio 1/OB1.cpp
+++ Ejercicio 1/OB1.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
-char nombre[12], apellido[12];
-int dia_nac, mes_nac, anio_nac, DNI, dia_elec= 30, mes_elec= 9, anio_elec= 2025;
-
+char nombre[24], apellido[24];
+int dia_nac, mes_nac, anio_nac, edad, DNI;
+const int dia_elec= 30, mes_elec= 9, anio_elec= 2025;
 main(){
-	
 	printf("Ingrese el nombre: ");
 	scanf("%s", &nombre);
 	printf("Ingrese el apellido: ");
@@ -17,14 +16,15 @@ main(){
 	printf("Ingrese el anio de nacimiento: ");
 	scanf("%d", &anio_nac);
 	
-	if(((anio_elec - anio_nac) >= 16) && (mes_nac <= mes_elec) && (dia_nac <= dia_elec)){
+	edad = anio_elec - anio_nac;
+	
+	if((edad >= 16) && (mes_nac <= mes_elec) && (dia_nac <= dia_elec)){
+		
 		printf("\n\n%s %s con DNI: %d.\nEsta empadronado para votar. ", nombre, apellido, DNI);
-		if(((anio_elec - anio_nac) >= 16 && (anio_elec - anio_nac) < 18) || (anio_elec - anio_nac) >= 80 ){
-			printf("\nUsted esta en el rango opcional. Desea votar, ingrese 1. Si no, ingrese 0.\n");
-		}
+		
 	}
 		else{
-			printf("\n\n%s %s con DNI: %d.\nNo esta empadronado para votar.", nombre, apellido, DNI);	
+			printf("\n\n%s %s con DNI: %d.\nNo cumple los requisitos para votar.", nombre, apellido, DNI);	
 		}
 }
 	
